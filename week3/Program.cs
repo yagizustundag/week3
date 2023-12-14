c1bdca2993b0a3b92f60c256eeddc4d6bf83eae8
Your Name <you@example.com>
2023-12-14 14:33:12 +0300

    week3

---
+++ week3/Program.cs
@@ -0,0 +1,30 @@
+﻿int WeekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+Console.WriteLine(WeekDay);
+
+switch (WeekDay)
+{
+    case 0:
+        Console.WriteLine("Sunday");
+        break;
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    default:
+        Console.WriteLine("Invalid value");
+        break;
+}
---
+++ week32/Program.cs
@@ -0,0 +1,33 @@
+﻿//Odd or even 
+
+int a = 11;
+
+int result = a % 2;
+
+//if (result == 0)
+//{
+//    Console.WriteLine($"{a} is an even number");
+//}
+//else
+//{
+//    Console.WriteLine($"{a} is an odd number");
+//}
+
+switch (result)
+{
+    case 0:
+        {
+            Console.WriteLine($"{a} is an even number");
+            break;
+        }
+    case 1:
+        {
+            Console.WriteLine($"{a} is an odd number");
+            break;
+        }
+    default:
+        {
+            Console.WriteLine("ERROR");
+            break;
+        }
+}
